1921ea70ed109f191b40400b1655d8b10867e4ac
Andrej Mlyncar <a.mlyncar@gmail.com>
2017-03-28 20:19:58 +0200

    Message removal interpret and message add placement impl

--- com.mlyncar.dp.interpreter/src/com/mlyncar/dp/interpreter/core/impl/UmlModelInterpreter.java
+++ com.mlyncar.dp.interpreter/src/com/mlyncar/dp/interpreter/core/impl/UmlModelInterpreter.java
@@ -2,12 +2,15 @@ package com.mlyncar.dp.interpreter.core.impl;
 
 import java.io.IOException;
 
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.gmf.runtime.diagram.core.services.ViewService;
+import org.eclipse.gmf.runtime.notation.Anchor;
 import org.eclipse.gmf.runtime.notation.Bounds;
 import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.gmf.runtime.notation.Edge;
+import org.eclipse.gmf.runtime.notation.IdentityAnchor;
 import org.eclipse.gmf.runtime.notation.NotationFactory;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.papyrus.infra.gmfdiag.css.notation.CSSDiagram;
@@ -86,6 +89,7 @@ public class UmlModelInterpreter extends AbstractInterpreter {
 		ActionExecutionSpecification actionSpec = getStartExecutionSpecification(interaction, change, sourceLifeline);
 		ActionExecutionSpecification newActionSpec = UMLFactory.eINSTANCE.createActionExecutionSpecification();
 		newActionSpec.setName("execSpecNew_" + nodeToAdd.getCreateEdge().getName());
+		targetLifeline.getCoveredBys().add(newActionSpec);
 		
 		MessageOccurrenceSpecification messageOccurrenceStart = UMLFactory.eINSTANCE.createMessageOccurrenceSpecification();
 		messageOccurrenceStart.setName("msgOccurrenceStart_" + nodeToAdd.getCreateEdge().getName());
@@ -132,9 +136,26 @@ public class UmlModelInterpreter extends AbstractInterpreter {
 		messageOccurrenceReplyStart.setMessage(newReplyMessage);
 		messageOccurrenceReplyEnd.setMessage(newReplyMessage);
 		
-		//getLifelineView();
-		addMessage(newMessage, getLifelineView(sourceLifeline.getName()), getLifelineView(targetLifeline.getName()), false);
-		addMessage(newReplyMessage, getLifelineView(targetLifeline.getName()), getLifelineView(sourceLifeline.getName()), true);
+		View sourceLifelineView = getLifelineView(sourceLifeline.getName());
+		View targetLifelineView = getLifelineView(targetLifeline.getName());
+		
+		org.eclipse.gmf.runtime.notation.Node executionViewInit = addActionExecution(sourceLifelineView, actionSpec);
+		org.eclipse.gmf.runtime.notation.Node executionViewEnd = addActionExecution(targetLifelineView, newActionSpec);
+		
+		Bounds location1 = NotationFactory.eINSTANCE.createBounds();
+		location1.setX(31);
+		location1.setY(calculateMessagePossition(nodeToAdd));
+		
+		Bounds location2 = NotationFactory.eINSTANCE.createBounds();
+		location2.setX(31);
+		location2.setY(calculateMessagePossition(nodeToAdd));
+		
+		executionViewInit.setLayoutConstraint(location1);
+		executionViewEnd.setLayoutConstraint(location2);
+		
+		addMessage(newMessage, executionViewInit, executionViewEnd, false);
+		addMessage(newReplyMessage, executionViewEnd, executionViewInit, true);
+		storeModelResource();
 		storeNotationResource();
 		logger.debug("Message add interpreted to uml model");
 	}
@@ -145,13 +166,50 @@ public class UmlModelInterpreter extends AbstractInterpreter {
 		Lifeline newLifeline = interaction.createLifeline(change.getNewValue().getName());
 		newLifeline.setInteraction(interaction);
 		addLifeline(newLifeline);
+		storeModelResource();
 		storeNotationResource();
 		logger.debug("Lifeline add interpreted to uml model");
 	}
 
 	@Override
-	protected void interpretMessageRemove(Change change) throws InterpreterException{
-		//interaction.getMessage(change.getNewValue().getCreateEdge().getName()).destroy();
+	protected void interpretMessageRemove(Change change) throws InterpreterException {
+		if(change.getNewValue().getCreateEdge().getEdgeType().equals(EdgeType.RETURN)) {
+			return;
+		}
+		Node nodeToRemove = change.getNewValue();
+		Node nodeToRemoveReturn = null;
+		for(Node node : nodeToRemove.childNodes()) {
+			if(node.isReply()) {
+				nodeToRemoveReturn = node;
+			}
+		}
+		if(nodeToRemoveReturn == null) {
+			throw new InterpreterException("Unable to interpret message " + nodeToRemove.getCreateEdge().getName() + " because it does not contain return message");
+		}
+		Edge edgeToRemove = null;
+		Edge edgeToRemoveReturn = null;
+		for(Object edge : diagram.getEdges()) {
+			Edge edgeView = (Edge) edge;
+			if(edgeView.getElement() instanceof Message) {
+				Message messageElement = (Message) edgeView.getElement();
+				if(messageElement.getName().equals(nodeToRemove.getCreateEdge().getName())) {
+					edgeToRemove = edgeView;
+				}
+				if(messageElement.getName().equals(nodeToRemoveReturn.getCreateEdge().getName())) {
+					edgeToRemoveReturn = edgeView;
+				}
+			}
+		}
+		if(edgeToRemove == null || edgeToRemoveReturn == null) {
+			throw new InterpreterException("Message to remove " + nodeToRemove.getCreateEdge().getName() + " or its return message not found in diagram");
+		}
+		
+		diagram.removeEdge(edgeToRemove);
+		diagram.removeEdge(edgeToRemoveReturn);
+		interaction.getMessage(nodeToRemove.getCreateEdge().getName()).destroy();
+		interaction.getMessage(nodeToRemoveReturn.getCreateEdge().getName()).destroy();
+		storeNotationResource();
+		storeModelResource();
 	}
 
 	@Override
@@ -162,7 +220,24 @@ public class UmlModelInterpreter extends AbstractInterpreter {
 
 	@Override
 	protected void interpretLifelineRemove(Change change) throws InterpreterException {
+		View lifelineCompartment = (View) getLifelineCompartment();
+		View lifelineToRemove = null;
+		for(Object obj : lifelineCompartment.getChildren()) {
+			View lifelineView = (View) obj;
+			if(lifelineView.getElement() instanceof Lifeline) {
+				Lifeline lifeline = (Lifeline) lifelineView.getElement();
+				if(lifeline.getName().equals(change.getNewValue().getName())) {
+					lifelineToRemove = lifelineView;
+				}
+			}
+		}
+		if(lifelineToRemove == null) {
+			throw new InterpreterException("Lifeline to remove " + change.getNewValue().getName() + " not found in diagram");
+		}
+		//lifelineCompartment.removeChild(lifelineToRemove);
+		//storeNotationResource();
 		//interaction.getLifeline(change.getNewValue().getName()).destroy();
+		//storeModelResource();
 	}
 
 	@Override
@@ -176,7 +251,6 @@ public class UmlModelInterpreter extends AbstractInterpreter {
 	}
 	
 	private ActionExecutionSpecification getStartExecutionSpecification(Interaction interaction, Change change, Lifeline sourceLifeline) {
-
 		MessageOccurrenceSpecification parentMsgOccurenceSpec = getParentMessageOccurence(interaction, change);
 		if(parentMsgOccurenceSpec != null) {
 			for(InteractionFragment fragment : interaction.getFragments()) {
@@ -225,30 +299,53 @@ public class UmlModelInterpreter extends AbstractInterpreter {
 	}
 	
 	private View addLifeline(Lifeline lifeline) {
-		View compartment1 = (View) diagram.getChildren().get(0);
-		Object compartment = compartment1.getChildren().get(1);
+		Object compartment = getLifelineCompartment();
 		final String nodeType = UMLVisualIDRegistry.getType(org.eclipse.papyrus.uml.diagram.sequence.edit.parts.LifelineEditPart.VISUAL_ID);
 		org.eclipse.gmf.runtime.notation.Node lifelineView = ViewService.createNode((View) compartment, lifeline, nodeType, UMLDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT);
 		Bounds location = NotationFactory.eINSTANCE.createBounds();
-		location.setX(300);
-		location.setY(40);
-		if(lifelineView instanceof Node) {
-			lifelineView.setLayoutConstraint(location);
-		}
+		location.setX(750); //lastlifeline + 100
+		location.setY(10);
+		location.setHeight(650);
+		location.setWidth(78);
+		lifelineView.setLayoutConstraint(location);
 		return lifelineView;
 	}
 	
+	private org.eclipse.gmf.runtime.notation.Node addActionExecution(View lifelineView, ActionExecutionSpecification specification) {
+		final String nodeType = UMLVisualIDRegistry.getType(org.eclipse.papyrus.uml.diagram.sequence.edit.parts.ActionExecutionSpecificationEditPart.VISUAL_ID);
+		org.eclipse.gmf.runtime.notation.Node executionView = ViewService.createNode(lifelineView, specification, nodeType, UMLDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT);
+		return executionView;
+	}
+	
+	private Object getLifelineCompartment() {
+		View compartment1 = (View) diagram.getChildren().get(0);
+		return compartment1.getChildren().get(1);
+	}
+	
 	private void addMessage(Message message, View lifelineSrcV, View lifelineDstV, boolean isReply) {
+		
 		View messageView;
+		IdentityAnchor idenAnchor = NotationFactory.eINSTANCE.createIdentityAnchor();
+
 		if(isReply) {
 			messageView = sequenceDiagViewProvider.createMessage_4005(message, diagram, -1, true,
 					UMLDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT);
+			Edge messageEdge = (Edge) messageView;
+			IdentityAnchor anch1 = (IdentityAnchor) messageEdge.createTargetAnchor(idenAnchor.eClass());
+			IdentityAnchor anch2 = (IdentityAnchor) messageEdge.createSourceAnchor(idenAnchor.eClass());
+			anch1.setId("(0.0,1.0){32}");
+			anch2.setId("(0.0,1.0){32}");
 		} else {
 			messageView = sequenceDiagViewProvider.createMessage_4003(message, diagram, -1, true,
-					UMLDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT);				
+					UMLDiagramEditorPlugin.DIAGRAM_PREFERENCES_HINT);	
+			Edge messageEdge = (Edge) messageView;
+			IdentityAnchor anch1 = (IdentityAnchor) messageEdge.createTargetAnchor(idenAnchor.eClass());
+			IdentityAnchor anch2 = (IdentityAnchor) messageEdge.createSourceAnchor(idenAnchor.eClass());
+			anch1.setId("(0.0,1.0){8}");
+			anch2.setId("(0.0,1.0){8}");
 		}
+		
 		if(messageView instanceof Edge) {
-			//there should be action execution specs;
 			((Edge)messageView).setSource(lifelineSrcV);
 			((Edge)messageView).setTarget(lifelineDstV);
 		}
@@ -256,10 +353,43 @@ public class UmlModelInterpreter extends AbstractInterpreter {
 	
 	private void storeNotationResource() throws InterpreterException {
 		try {
-			this.resource.save(null);
 			this.notationResource.save(null);
 		} catch (IOException e) {
 			throw new InterpreterException("Unable to update notation resource", e);
 		}
+	}
+	
+	private void storeModelResource() throws InterpreterException {
+		try {
+			this.resource.save(null);
+		} catch (IOException e) {
+			throw new InterpreterException("Unable to update model resource", e);
+		}
+	}
+	
+	private Integer calculateMessagePossition(Node newValue) {
+		if(newValue.getLeftSibling() == null) {
+			logger.debug("Sibling not found, returning 30");
+			return 30;
+		}
+		View lifelineView = getLifelineView(newValue.getParentNode().getName());
+		for(Object viewObj : lifelineView.getChildren()) {
+			View view = (View) viewObj;
+			if(view.getElement() != null && view.getElement() instanceof ActionExecutionSpecification) {
+				ActionExecutionSpecification specification = (ActionExecutionSpecification) view.getElement();
+				if(specification.getStart() instanceof MessageOccurrenceSpecification) {
+					logger.debug("Checking msg occurence for sibling execution " + specification.getStart().getName());
+					String messageName = ((MessageOccurrenceSpecification) specification.getStart()).getMessage().getName();
+					logger.debug("Message name " + messageName);
+					if(messageName.equals(newValue.getLeftSibling().getCreateEdge().getName())) {
+						org.eclipse.gmf.runtime.notation.Node node = (org.eclipse.gmf.runtime.notation.Node) viewObj;
+						Bounds bound = (Bounds) node.getLayoutConstraint();
+						logger.debug("Sibling found, calculated following Y value " + (bound.getY() + bound.getHeight() + 10));
+						return bound.getY() + bound.getHeight() + 10;
+					}	
+				}
+			}
+		}
+		return 30;
 	}
 }
